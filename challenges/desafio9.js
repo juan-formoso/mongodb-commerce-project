c4ab3fdf21f743fbc133abcd5e64e23434757389
Avery <trajano.juan@gmail.com>
2022-04-07 22:43:28 -0300

    Finalizando requisito 9

---
+++ challenges/desafio9.js
@@ -0,0 +1,7 @@
+// Retorne o nome de todos os lanches que possuam calorias abaixo de 500.
+db.produtos
+  .find(
+    { "valoresNutricionais.0.quantidade": { $lt: 500 } },
+    { nome: 1, _id: 0 },
+  )
+  .pretty();
